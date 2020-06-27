af7f6799eca91591165f128cc6db1ef3ea12d143
Daniel Manners <daniel.a.manners@gmail.com>
2020-06-27 18:06:46 -0400

    Fixed how qemu-img convert was processing files; added image decompression. (#3)

--- lib/facter/proxmox_cluster_nextid.rb
+++ lib/facter/proxmox_cluster_nextid.rb
@@ -1,5 +1,6 @@
 # Get the next VM ID for the Proxmox Cluster.
 Facter.add('proxmox_cluster_nextid') do
+    confine :exists => "/usr/bin/pvesh"
     setcode do
         Facter::Core::Execution.execute('/usr/bin/pvesh get /cluster/nextid')
     end                
